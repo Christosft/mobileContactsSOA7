81f26e51ba3f9ffdd416cfda78e256fdd7ef22fe
Chris Ftoulis <christosftoulis3@gmail.com>
2025-02-02 22:46:06 +0200

    update main in contasts update, search end Phone book, update code not working yet

--- src/gr/aueb/cf/mobilecontacts/Main.java
+++ src/gr/aueb/cf/mobilecontacts/Main.java
@@ -2,7 +2,10 @@ package gr.aueb.cf.mobilecontacts;
 
 import gr.aueb.cf.mobilecontacts.controller.MobileContactController;
 import gr.aueb.cf.mobilecontacts.dto.MobileContactInsertDTO;
+import gr.aueb.cf.mobilecontacts.dto.MobileContactUpdateDTO;
 
+
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -51,19 +54,75 @@ public class Main {
                 break;
 
             case "2":
-                //
+                System.out.println("Please enter the contact ID to update:");
+                Long contactId = null;
+
+                try {
+                    contactId = Long.parseLong(getToken());
+
+                } catch (NumberFormatException e) {
+                   System.out.println("Invalid ID. Please enter a valid numeric ID.");
+                }
+
+                System.out.println("Contact ID to update: " + contactId);
+
+                System.out.println("Please please enter new firstname lastname and phone number to update");
+                firstname = getToken();
+                lastname = getToken();
+                phoneNumber = getToken();
+                MobileContactUpdateDTO updateDTO = new MobileContactUpdateDTO(contactId, firstname, lastname, phoneNumber);
+                response = controller.updateContact(updateDTO);
+
+                if (response.startsWith("OK")) {
+                    System.out.println("\nUpdate succeeded");
+                    System.out.println(response.substring(3));
+                } else {
+                    System.out.println("\nUpdate not succeeded");
+                    System.out.println(response.substring(6));
+                }
                 break;
 
             case "3":
-                //
+                System.out.println("Please insert contact phone number to delete");
+                phoneNumber = getToken();
+                response = controller.deleteContactByPhoneNumber(phoneNumber);
+
+                if (response.startsWith("OK")) {
+                    System.out.println("\nDelete succeeded");
+                    System.out.println(response.substring(3));
+                } else {
+                    System.out.println("\nDelete is not succeeded");
+                    System.out.println(response.substring(6));
+                }
                 break;
 
             case "4":
-                //
+
+                System.out.println("Please select contact by phone Number");
+                phoneNumber = getToken();
+
+                response = controller.getContactByPhoneNumber(phoneNumber);
+
+                if (response.startsWith("OK")) {
+                    System.out.println("\nContact search succeeded");
+                    System.out.println(response.substring(3));
+                } else {
+                    System.out.println("\nContact search is not succeeded");
+                    System.out.println(response.substring(6));
+                }
                 break;
 
+
             case "5":
-                //
+                System.out.println("Phone book:");
+                List<String> contacts = controller.getAllContacts();
+                if (contacts.isEmpty()) {
+                    System.out.println("No contacts available.");
+                } else {
+                    for (String contact : contacts) {
+                        System.out.println(contact);
+                    }
+                }
                 break;
 
             default:
